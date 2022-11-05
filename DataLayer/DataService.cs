5431146b065bb2bd826fc20407e2d5d796c56116
felexmose <felixmose@msn.com>
2022-11-05 13:06:04 +0100

    done

---
+++ DataLayer/DataService.cs
@@ -0,0 +1,111 @@
+﻿using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class DataService : IDataService
+    {
+        private readonly NorthwindContext _db;
+        public DataService()
+        {
+            this._db = new NorthwindContext();
+        }
+
+        public Category CreateCategory(string name, string description)
+        {
+            var category = new Category() { CategoryId = 9, Name = name, Description = description };
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+            
+            return _db.Categories.Where(x => x.Name == name).FirstOrDefault()!;
+        }
+
+        public bool DeleteCategory(int id)
+        {
+            var cat = _db.Categories.Where(x => x.CategoryId == id).FirstOrDefault();
+            if (cat == null)
+            {
+                return false;
+            }
+            _db.Categories.Remove(cat);
+            _db.SaveChanges();
+            
+            return true;
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            return _db.Categories.ToList();
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            return _db.Orders.ToList();
+        }
+
+        public Category GetCategoryById(int categoryId)
+        {
+           return _db.Categories.Where(x => x.CategoryId == categoryId).FirstOrDefault()!;
+        }
+
+        // fix OrderDetails issue
+        public List<OrderDetails> GetDetailsForSpecificProductId(string productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetOrderById(int id)
+        {
+            return _db.Orders.Where(x => x.Id == id).FirstOrDefault()!;
+        }
+
+        public List<Order> GetOrderByShippingName(string shippingName)
+        {
+            return _db.Orders.Where(x => x.ShipName == shippingName).ToList();
+        }
+
+        // fix OrderDetails issue
+        public List<OrderDetailsWithProductName> GetOrderDetailsByOrderId(int orderId)
+        {
+            //return _db.OrderDetails
+            //    .Where(x => x.Order.Id == orderId)
+            //    .Select(x => new OrderDetailsWithProductName { ProductName = x.Product.Name, UnitPrice = x.UnitPrice , Quantity = x.Quantity })
+            //    .ToList();
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductById(int productId)
+        {
+            return _db.Products.Where(x => x.Id == productId).FirstOrDefault()!;
+        }
+
+        public List<Product> GetProductsByCategoryId(int categoryId)
+        {
+            return _db.Products.Where(x => x.CategoryId == categoryId).ToList();
+        }
+
+        // return new object with product name and category name??
+        public List<Product> GetProductsThatContainSubstring(string substring)
+        {
+            return _db.Products.Where(x => x.Name.Contains(substring)).ToList();
+        }
+
+        public bool UpdateCategory(int id, string name, string description)
+        {
+
+            var cat = _db.Categories.Where(x => x.CategoryId == id).FirstOrDefault();
+
+            if (cat == null)
+            {
+                return false;
+            }
+            //var cat2Delete = new Category() { CategoryId = id, Name = name, Description = description };
+            _db.Categories.Remove(cat);
+            var newCat = new Category() {CategoryId = id, Name = name, Description = description };
+            _db.Categories.Add(newCat);
+            _db.SaveChanges();
+            
+            return true;
+        }
+    }
+}
---
+++ DataLayer/NorthwindContext.cs
@@ -0,0 +1,72 @@
+﻿using DataLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DataLayer
+{
+    public class NorthwindContext : DbContext
+    {
+        const string ConnectionString = "host=localhost;db=Northwind;uid=postgres;pwd=23278320";
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }        
+        //public DbSet<OrderDetails> OrderDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            //optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+            optionsBuilder.UseNpgsql(ConnectionString);
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<OrderDetails>().HasKey(am => new
+            //{
+            //    am.OrderId,
+            //    am.ProductId,
+            //});
+
+            //modelBuilder.Entity<OrderDetails>().HasOne(m => m.Product)
+            //.WithMany(am => am.).HasForeignKey(m => m.ProductId);
+
+            //modelBuilder.Entity<OrderDetails>().HasOne(a => a.Order)
+            //.WithMany(at => at.Product_Orders).HasForeignKey(a => a.OrderId);
+
+            base.OnModelCreating(modelBuilder);
+
+
+
+            modelBuilder.Entity<Category>().ToTable("categories");
+            modelBuilder.Entity<Category>().Property(x => x.CategoryId).HasColumnName("categoryid");
+            modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("categoryname");
+            modelBuilder.Entity<Category>().Property(x => x.Description).HasColumnName("description");
+
+            modelBuilder.Entity<Product>().ToTable("products");
+            modelBuilder.Entity<Product>().Property(x => x.Id).HasColumnName("productid");
+            modelBuilder.Entity<Product>().Property(x => x.Name).HasColumnName("productname");
+            modelBuilder.Entity<Product>().Property(x => x.UnitPrice).HasColumnName("unitprice");
+            modelBuilder.Entity<Product>().Property(x => x.QuantityPerUnit).HasColumnName("quantityperunit");
+            modelBuilder.Entity<Product>().Property(x => x.UnitsInStock).HasColumnName("unitsinstock");
+            modelBuilder.Entity<Product>().Property(x => x.CategoryId).HasColumnName("categoryid");
+
+            modelBuilder.Entity<Order>().ToTable("orders");
+            modelBuilder.Entity<Order>().Property(x => x.Id).HasColumnName("orderid");
+            modelBuilder.Entity<Order>().Property(x => x.Date).HasColumnName("orderdate");
+            modelBuilder.Entity<Order>().Property(x => x.Require).HasColumnName("requireddate");
+            modelBuilder.Entity<Order>().Property(x => x.Shipped).HasColumnName("shippeddate");
+            modelBuilder.Entity<Order>().Property(x => x.Freight).HasColumnName("freight");
+            modelBuilder.Entity<Order>().Property(x => x.ShipName).HasColumnName("shipname");
+            modelBuilder.Entity<Order>().Property(x => x.ShipCity).HasColumnName("shipcity");
+
+            //modelBuilder.Entity<OrderDetails>().ToTable("orderdetails");
+            //modelBuilder.Entity<OrderDetails>().Property(x => x.OrderId).HasColumnName("orderid");
+            //modelBuilder.Entity<OrderDetails>().Property(x => x.ProductId).HasColumnName("productid");
+            //modelBuilder.Entity<OrderDetails>().Property(x => x.UnitPrice).HasColumnName("unitprice");
+            //modelBuilder.Entity<OrderDetails>().Property(x => x.Quantity).HasColumnName("quantity");
+            //modelBuilder.Entity<OrderDetails>().Property(x => x.Discount).HasColumnName("discount");
+        }
+    }
+}
